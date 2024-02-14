24127e28aeb413be34195aab074348c227f162bb
Nguyen Quang Minh <E1155965@u.nus.edu>
2024-02-14 13:46:43 +0800

    worked on pset4

--- Problem Sets/ps4/src/SGTree.java
+++ Problem Sets/ps4/src/SGTree.java
@@ -11,6 +11,34 @@ import java.util.Arrays;
  * a subtree.
  */
 
+//Simple array-based stack for iterative implementations
+class Stack<T> {
+    private T[] m_a;
+    //Pos to insert into next
+    private int m_pos;
+
+    @SuppressWarnings("unchecked")
+    public Stack(int length) {
+        m_a = (T[]) new Object[length];
+    }
+
+    public T pop() {
+        m_pos--;
+        T temp = m_a[m_pos];
+        m_a[m_pos] = null;
+        return temp;
+    }
+
+    public void push(T e) {
+        m_a[m_pos] = e;
+        m_pos++;
+    }
+
+    public boolean isEmpty() {
+        return m_a[0] == null;
+    }
+}
+
 public class SGTree {
 
     // Designates which child in a binary tree
@@ -73,7 +101,7 @@ public class SGTree {
      * @return array of nodes
      */
     // Told you I'd find an iterative solution, albeit at 2am and now im dead
-    public TreeNode[] enumerateNodes(TreeNode node, Child child) {
+    public TreeNode[] enumerateNodesIterative(TreeNode node, Child child) {
         int nodesCount = countNodes(node, child);
         TreeNode[] nodes = new TreeNode[nodesCount];
         int nodesPos = 0;
@@ -127,6 +155,27 @@ public class SGTree {
         return nodes;
     }
 
+    public TreeNode[] enumerateNodes(TreeNode node, Child child) {
+        TreeNode[] ans = new TreeNode[countNodes(node, child)];
+        // Point node to the correct side
+        node = child == Child.LEFT ? node.left : node.right;
+        enumerateNodesRecursive(node, ans, 0);
+        return ans;
+    }
+
+    public int enumerateNodesRecursive(TreeNode node, TreeNode[] ans, int ansPos) {
+        if (node == null)
+            return ansPos;
+
+        ansPos = enumerateNodesRecursive(node.left, ans, ansPos);
+
+        ans[ansPos] = node;
+        ansPos++;
+
+        ansPos = enumerateNodesRecursive(node.right, ans, ansPos);
+        return ansPos;
+    }
+
     /**
      * Builds a tree from the list of nodes
      * Returns the node that is the new root of the subtree
@@ -134,6 +183,79 @@ public class SGTree {
      * @param nodeList ordered array of nodes
      * @return the new root node
      */
+    public TreeNode buildTreeIterative(TreeNode[] nodeList) {
+        if (nodeList.length == 1)
+            return nodeList[0];
+        if (nodeList.length == 0)
+            return null;
+
+        //Guaranteed to not overflow because max height = log(nodeList.lenght)
+        Stack<Integer> lows = new Stack<>(nodeList.length);
+        Stack<Integer> highs = new Stack<>(nodeList.length);
+        Stack<TreeNode> nodes = new Stack<>(nodeList.length);
+
+        int low = 0;
+        int high = nodeList.length - 1;
+
+        int i_mid = low + (high - low) / 2;
+        TreeNode root = nodeList[i_mid];
+        TreeNode cur = root;
+
+        lows.push(i_mid + 1);
+        highs.push(high);
+
+        high = i_mid - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            cur.left = nodeList[mid];
+
+            lows.push(mid + 1);
+            highs.push(high);
+            nodes.push(cur);
+
+            cur = cur.left;
+
+            high = mid - 1;
+        }
+
+        cur = cur.left;
+
+        while(!nodes.isEmpty()) {
+            cur = nodes.pop();
+            low = lows.pop();
+            high = highs.pop();
+
+            if (low <= high) {
+                int l_mid = low + (high - low) / 2;
+                cur.right = nodeList[l_mid];
+                cur = cur.right;
+
+                lows.push(l_mid + 1);
+                highs.push(high);
+
+                high = l_mid - 1;
+            }
+
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                cur.left = nodeList[mid];
+
+                lows.push(mid + 1);
+                highs.push(high);
+                nodes.push(cur);
+
+                cur = cur.left;
+
+                high = mid - 1;
+            }
+
+            cur = cur.left;
+        }
+
+        return root;
+    }
+
     public TreeNode buildTree(TreeNode[] nodeList) {
         if (nodeList.length == 0)
             return null;
@@ -142,7 +264,7 @@ public class SGTree {
         mid.left = buildTree(Arrays.copyOfRange(nodeList, 0, nodeList.length / 2));
         mid.right = buildTree(Arrays.copyOfRange(nodeList,nodeList.length / 2 + 1, nodeList.length));
         return mid;
-    }
+        }
 
     /**
     * Rebuilds the specified subtree of a node
@@ -199,12 +321,17 @@ public class SGTree {
     // Simple main function for debugging purposes
     public static void main(String[] args) {
         SGTree tree = new SGTree();
-        for (int i = -10; i < 100; i++) {
+        for (int i = 0; i < 4; i++) {
             tree.insert(i);
         }
+
+        TreeNode[] ls = tree.enumerateNodes(tree.root, Child.RIGHT);
+        System.out.println(Arrays.toString(ls));
+        //TreeNode root = tree.buildTreeIterative(ls);
+        TreeNode root2 = tree.buildTree(ls);
 //        tree.rebuild(tree.root, Child.RIGHT);
 //        TreeNode bruh = tree.buildTree(tree.enumerateNodes(tree.root, Child.RIGHT));
-        System.out.println(Arrays.toString(tree.enumerateNodes(tree.root, Child.RIGHT)));
+        System.out.println(Arrays.toString(tree.enumerateNodesIterative(root2, Child.LEFT)));
 //        System.out.println(Arrays.toString(tree.enumerateNodes(tree.root, Child.RIGHT)));
     }
 }
